569ae8e5561457f38f2d9baa6d316bd0c9eb3db0
qemu <requests567@gmail.com>
2026-09-26 09:03:42 +0300

    tung tung sahurr
    sahurr

--- src/main.c
+++ src/main.c
@@ -1,7 +1,7 @@
 #define _POSIX_C_SOURCE 200809L
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+#include <unistd.h> // tung tung sahirr
 #include <string.h>
 
 #define TRY(cmd) do { \
